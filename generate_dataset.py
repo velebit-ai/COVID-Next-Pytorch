b59c07761603dba1936e268eea9c802284e21fbd
Ivan Borko <ivan.borko8@gmail.com>
2021-03-10 17:01:04 +0100

    fix: covid chestray dataset has a new format of metadata

--- generate_dataset.py
+++ generate_dataset.py
@@ -45,7 +45,7 @@ def main(args):
     mapping['SARS'] = 'pneumonia'
     mapping['MERS'] = 'pneumonia'
     mapping['Streptococcus'] = 'pneumonia'
-    mapping['Normal'] = 'normal'
+    mapping['No Finding'] = 'normal'
     mapping['Lung Opacity'] = 'pneumonia'
     mapping['1'] = 'pneumonia'
 
@@ -53,8 +53,9 @@ def main(args):
     covid_csv = os.path.join(args.covid_dir, "metadata.csv")
 
     csv = pd.read_csv(covid_csv, nrows=None)
-    idx_pa = csv["view"] == "PA"
-    csv = csv[idx_pa]
+    csv = csv[csv["view"] == "PA"]
+    log.info("Metadata contains {} items with PA".format(len(csv)))
+
     pneumonias = ["COVID-19", "SARS", "MERS", "ARDS", "Streptococcus"]
     pathologies = ["Pneumonia", "Viral Pneumonia", "Bacterial Pneumonia",
                    "No Finding"] + pneumonias
@@ -62,11 +63,11 @@ def main(args):
 
     filename_label = {'normal': [], 'pneumonia': [], 'COVID-19': []}
     count = {'normal': 0, 'pneumonia': 0, 'COVID-19': 0}
-    for index, row in csv.iterrows():
-        f = row['finding']
+    for row in csv.itertuples():
+        f = row.finding.split('/')[-1]
         if f in mapping:
             count[mapping[f]] += 1
-            entry = [int(row['patientid']), row['filename'], mapping[f]]
+            entry = [row.patientid, row.filename, mapping[f]]
             filename_label[mapping[f]].append(entry)
 
     log.info('Data distribution from covid-chestxray-dataset:')
@@ -82,7 +83,7 @@ def main(args):
         patient_ids = np.unique(arr[:, 0])
         test_size = int(len(patient_ids) * args.test_size)
         test_patients = np.random.choice(patient_ids, test_size, replace=False)
-        log.info('Category: {}, N test patients'.format(key, test_size))
+        log.info('Category: {}, N test patients {}'.format(key, test_size))
 
         # go through all the patients
         for patient in arr:
@@ -111,13 +112,13 @@ def main(args):
     csv_pneu = pd.read_csv(kaggle_csv_pneu, nrows=None)
     patients = {'normal': [], 'pneumonia': []}
 
-    for index, row in csv_normal.iterrows():
-        if row['class'] == 'Normal':
-            patients['normal'].append(row['patientId'])
+    for row in csv_normal.itertuples():
+        if row[2] == 'Normal':
+            patients['normal'].append(row.patientId)
 
-    for index, row in csv_pneu.iterrows():
-        if int(row['Target']) == 1:
-            patients['pneumonia'].append(row['patientId'])
+    for row in csv_pneu.itertuples():
+        if row.Target == 1:
+            patients['pneumonia'].append(row.patientId)
 
     log.info("Preparing Kaggle dataset...")
     counter = 0
@@ -164,21 +165,26 @@ if __name__ == "__main__":
     np.random.seed(1337)
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--covid-dir',
-                        help="Path to the cloned `covid-chestxray-dataset` "
-                             "repo dir",
-                        type=str)
-    parser.add_argument('--kaggle-data',
-                        help="Path to the downloaded Kaggle dataset dir",
-                        type=str)
-    parser.add_argument('--save-path',
-                        help="Directory where to save the new COVIDx dataset",
-                        type=str)
-    parser.add_argument('--test-size',
-                        help="Test set size fraction. Defaults to 10%.",
-                        default=0.1,
-                        type=float)
+    parser.add_argument(
+        'covid_dir',
+        help="Path to the cloned `covid-chestxray-dataset` repo dir",
+        type=str)
+    parser.add_argument(
+        'kaggle_data',
+        help="Path to the downloaded Kaggle dataset dir",
+        type=str)
+    parser.add_argument(
+        'save_path',
+        help="Directory where to save the new COVIDx dataset",
+        type=str)
+    parser.add_argument(
+        '--test-size',
+        help="Test set size fraction. Defaults to 10%.",
+        default=0.1, type=float)
+
     args = parser.parse_args()
+
     if args.test_size < 0 or args.test_size > 1:
         raise ValueError("Test fraction value must be in range [0, 1]")
+
     main(args)
